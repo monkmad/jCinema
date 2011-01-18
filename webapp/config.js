8a2bcb0e0798cbc711883c41fc2af2d245dc1ad3
monkmad <monkmad@gmail.com>
2011-01-18 21:28:57 +0100

    Added patterns for WDLXTV STD & WALL sheet modes

--- webapp/config.js
+++ webapp/config.js
@@ -7,6 +7,6 @@ $.extend(jCinema.options, {
 	//MediaSearchPath: '/tmp/media/usb',
 	FolderImagePathPattern:     '{path}/{name}.jpg;{path}/folder.jpg',
 	ThumbnailImagePathPattern:  '{dir}/_MovieSheets/{name}/thumb.jpg;{dir}/{name}.jpg;{dir}/folder.jpg',
-	MovieSheetImagePathPattern: '{dir}/_MovieSheets/{name}/sheet.jpg;{dir}/{name}{suffix}_sheet.jpg;{dir}/wd_tv.jpg',
+	MovieSheetImagePathPattern: '{dir}/_MovieSheets/{name}/sheet.jpg;{dir}/{name}{suffix}_sheet.sheet.jpg;{dir}/{name}{suffix}_sheet.wall.jpg;{dir}/{name}{suffix}_sheet.jpg;{dir}/wd_tv.jpg',
 	PatternSeparator: 	    ';',
 });
